96d46eb33b184abdd3a12d72b2510cf810ea0122
David G <david.a.grs@gmail.com>
2017-07-13 21:51:59 +0200

    adding PoC mainwindow

---
+++ mainwindow.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <QtWidgets>
+
+class QWebView;
+class QLineEdit;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow();
+
+protected slots:
+
+    void adjustLocation();
+    void changeLocation();
+    void adjustTitle();
+    void setProgress(int p);
+    void finishLoading(bool);
+
+    void viewSource();
+    void slotSourceDownloaded();
+
+    void highlightAllLinks();
+    void rotateImages(bool invert);
+    void removeGifImages();
+    void removeInlineFrames();
+    void removeObjectElements();
+    void removeEmbeddedElements();
+
+private:
+    QString jQuery;
+    QWebView *view;
+    QLineEdit *locationEdit;
+    QAction *rotateAction;
+    int progress;
+};
