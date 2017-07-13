0bc1fdc2bd858486dbfc3b0dc09c60f4e0d089a6
David G <david.a.grs@gmail.com>
2017-07-13 21:30:15 +0200

    initial commit

---
+++ main.cc
@@ -0,0 +1,15 @@
+//#include "window.h"
+
+#include <QApplication>
+
+int main( int argc, char **argv )
+{
+    QApplication app(argc, argv);
+    app.setApplicationName("qtable");
+
+//   Ui::Window window;
+//    window.show();
+
+    return app.exec();
+}
+
