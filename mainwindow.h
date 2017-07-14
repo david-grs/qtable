955b7c3179b0b217174c47bd53900299eeb93e63
David G <david.a.grs@gmail.com>
2017-07-14 14:14:56 +0200

    cleanup

--- mainwindow.h
+++ mainwindow.h
@@ -13,27 +13,9 @@ public:
 	explicit MainWindow();
 
 protected slots:
-
-	void adjustLocation();
-	void changeLocation();
-	void adjustTitle();
-	void setProgress(int p);
 	void finishLoading(bool);
-
-	void viewSource();
-	void slotSourceDownloaded();
-
-	void highlightAllLinks();
-	void rotateImages(bool invert);
-	void removeGifImages();
-	void removeInlineFrames();
-	void removeObjectElements();
-	void removeEmbeddedElements();
 
 private:
 	QString jQuery;
 	QWebView *view;
-	QLineEdit *locationEdit;
-	QAction *rotateAction;
-	int progress;
 };
