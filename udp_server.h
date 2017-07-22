72be2d71297113b4965c20824c12d9d3829b5f13
David G <david.a.grs@gmail.com>
2017-07-22 23:55:38 +0200

    udp_server: adding callback

--- udp_server.h
+++ udp_server.h
@@ -2,14 +2,19 @@
 
 #include <boost/asio.hpp>
 
+#include <experimental/string_view>
+
 using udp = boost::asio::ip::udp;
 
 class udp_server
 {
 public:
-	udp_server(boost::asio::io_service& io_service, short port) :
+	using PacketHandler = std::function<void(std::experimental::string_view)>;
+
+	udp_server(boost::asio::io_service& io_service, short port, PacketHandler handler) :
 		mIOService(io_service),
-		mSocket(io_service, udp::endpoint(udp::v4(), port))
+		mSocket(io_service, udp::endpoint(udp::v4(), port)),
+		mHandler(handler)
 	{
 		async_receive();
 	}
@@ -20,13 +25,15 @@ private:
 		mSocket.async_receive_from(
 			boost::asio::buffer(mData, MaxLength),
 			mEndpoint,
-			[this](const boost::system::error_code& error, size_t bytes)
+			[this](const boost::system::error_code& error, std::size_t bytes)
 		{
 			if (error)
 			{
 				mSocket.close();
 				return;
 			}
+
+			mHandler(std::experimental::string_view(mData, bytes));
 
 			//if (bytes > 0)
 			//	std::cout << "received " << bytes <<  " bytes" << std::endl;
@@ -38,6 +45,8 @@ private:
 	boost::asio::io_service& mIOService;
 	udp::socket mSocket;
 	udp::endpoint mEndpoint;
+
+	PacketHandler mHandler;
 
 	enum { MaxLength = 1024 };
 	char mData[MaxLength];
