69efbfc80fe2866865b619a4584011108743c99b
David G <david.a.grs@gmail.com>
2017-08-02 23:33:09 +0200

    poc price_feeder

--- price_feeder.cc
+++ price_feeder.cc
@@ -1,123 +1,42 @@
-#include <string>
-
+#include "udp_server.h"
 #include "udp_client.h"
 #include "proto.h"
-#include "futils.h"
 
-#include <boost/regex.hpp>
-#include <boost/algorithm/string.hpp>
-#include <boost/optional.hpp>
-
-#include <array>
-#include <vector>
-#include <thread>
-#include <chrono>
-#include <experimental/string_view>
-#include <cassert>
-
-InstrumentDefinition parse_instrument(const std::string& str)
+int main( int argc, char **argv )
 {
-	std::string market, feedcode;
-	std::unordered_map<FIXTag, std::string> attributes;
-
-	bool expiryDate = false;
-
-	for (std::size_t i = 0; i < str.size(); ++i)
-	{
-		if (str[i] == 0x01)
-			continue;
-
-		const std::size_t startTag = i;
-		const std::size_t separator = str.find('=', i + 1);
-
-		if (separator == std::string::npos)
-			throw std::runtime_error("invalid security definition: malformed input");
-
-		const std::size_t tagSize = separator - startTag;
-		const std::size_t startValue = separator + 1;
-		const std::size_t nextPair = str.find(0x01, startValue + 1);
-
-		const std::size_t valueLength = nextPair != std::string::npos ? nextPair - startValue : str.size() - startValue;
-		i = nextPair;
-
-		std::experimental::string_view codeStr(str.c_str() + startTag, tagSize);
-		auto it = FIXTagsByCode.find(codeStr);
-		if (it != FIXTagsByCode.cend())
-		{
-			const FIXTag& tag = it->second;
-			FIXTagCode tagCode = tag.GetCode();
-
-			std::string value = str.substr(startValue, valueLength);
-			if (tagCode == FIXTagCode::EventType) // FIX
-			{
-				expiryDate = value == "7";
-			}
-			else if (tagCode == FIXTagCode::SecurityExchange)
-			{
-				market = std::move(value);
-			}
-			else if (tagCode == FIXTagCode::InstrumentName)
-			{
-				feedcode = std::move(value);
-			}
-			else if (tagCode != FIXTagCode::EventTime || expiryDate)
-			{
-				auto p = attributes.emplace(tag, std::move(value));
-
-				assert(p.second);
-				(void)p;
-			}
-		}
-	}
-
-	if (market.empty() || feedcode.empty())
-		throw std::runtime_error("invalid security definition: invalid market/feedcode");
-
-	return InstrumentDefinition(std::move(market), std::move(feedcode), std::move(attributes));
-}
-
-std::vector<InstrumentDefinition> load(const std::string& filename)
-{
-	auto start = std::chrono::steady_clock::now();
+	boost::asio::io_service io_service;
 
 	std::vector<InstrumentDefinition> instruments;
-	instruments.reserve(1000000);
 
-	std::string content = cpp::read_all(filename);
-
-	cpp::for_each_line(content, [&](const std::string& line)
+	udp_server serv(io_service, 1234, [&](std::experimental::string_view str)
 	{
-		InstrumentDefinition instr = parse_instrument(line);
-		if (instr.GetFeedcode().substr(0, 2) == "OZ")
-			instruments.push_back(std::move(instr));
-		//std::cout << instruments.back() << std::endl;
+		//std::cout << "received " << str.size() << " bytes" << std::endl;
+
+		std::stringstream strm;
+		strm.write(str.data(), str.size());
+
+		boost::archive::binary_iarchive archive(strm);
+
+		InstrumentDefinition instr;
+		archive >> instr;
+
+		instruments.push_back(std::move(instr));
 	});
 
-	auto end = std::chrono::steady_clock::now();
+	udp_client price_feed(io_service, "localhost", 1235);
 
-	std::cout << "loaded " << instruments.size() << " instruments after " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << std::endl;
-	return instruments;
-}
-
-std::vector<std::string> serialize_instruments(const std::vector<InstrumentDefinition>& instruments)
-{
-	std::vector<std::string> messages;
-	messages.reserve(instruments.size());
-
-	for (const InstrumentDefinition& instr : instruments)
+	while (true)
 	{
-		std::string msg = proto::serialize(instr);
-		messages.push_back(std::move(msg));//client.send(str);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		io_service.poll();
+
+		send_price();
 	}
 
-	return messages;
+	return 0;
 }
 
-void send_instrument_definitions(udp_client& client, const std::vector<std::string>& messages)
-{
-	for (const std::string& msg : messages)
-		client.send(msg);
-}
+
 
 int main()
 {
